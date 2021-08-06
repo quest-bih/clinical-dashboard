4e2929e2fb544137d47d3a71707c605a6600cba7
bgcarlisle <bgcarlisle@users.noreply.github.com>
2021-08-06 16:24:08 +0200

    Remove unnecessary code

--- umc_plots.R
+++ umc_plots.R
@@ -462,158 +462,93 @@ umc_plot_clinicaltrials_timpub_5a <- function (dataset, umc, rt, color_palette) 
         filter(published_5a) %>%
         nrow()
 
-    if ( umc != "all" ) {
-        ## If the selected UMC is not "all," calculate
-        ## the percentage
 
-        plot_data <- tribble(
-            ~year, ~umc_percentage, ~all_percentage
-        )
+    
+    plot_data <- tribble(
+        ~year, ~umc_percentage, ~all_percentage
+    )
 
-        for (current_year in years) {
+    for (current_year in years) {
 
-            umc_numer <-  dataset %>%
-                filter(
-                    city == umc,
-                    year == current_year,
-                    published_5a
-                ) %>%
-                nrow()
-
-            umc_denom <-  dataset %>%
-                filter(
-                    city == umc,
-                    year == current_year
-                ) %>%
-                nrow()
-
-            all_numer <-  dataset %>%
-                filter(
-                    year == current_year,
-                    published_5a
-                ) %>%
-                nrow()
-
-            all_denom <-  dataset %>%
-                filter(
-                    year == current_year
-                ) %>%
-                nrow()
-
-            umc_percentage <- 100*umc_numer/umc_denom
-            all_percentage <- 100*all_numer/all_denom
-
-            if (all_denom > 5) { ## This is because we only have 1
-                             ## data point in 2013 with 5 years of
-                             ## follow-up
-
-                plot_data <- plot_data %>%
-                    bind_rows(
-                        tribble(
-                            ~year, ~umc_percentage, ~all_percentage,
-                            current_year, umc_percentage, all_percentage
-                        )
-                    )
-            }
-            
-        }
-
-        plot_ly(
-            plot_data,
-            x = ~year,
-            y = ~umc_percentage,
-            name = umc,
-            type = 'scatter',
-            mode = 'lines+markers',
-            marker = list(
-                color = color_palette[3],
-                line = list(
-                    color = 'rgb(0,0,0)',
-                    width = 1.5
-                )
-            )
-        ) %>%
-            add_trace(
-                y=~all_percentage,
-                name='All',
-                marker = list(color = color_palette[2])
+        umc_numer <-  dataset %>%
+            filter(
+                city == umc,
+                year == current_year,
+                published_5a
             ) %>%
-            layout(
-                xaxis = list(
-                    title = '<b>Completion year</b>',
-                    dtick = 1
-                ),
-                yaxis = list(
-                    title = '<b>Reported within 5 years (%)</b>',
-                    range = c(0, 100)
-                ),
-                paper_bgcolor = color_palette[9],
-                plot_bgcolor = color_palette[9],
-                legend = list(xanchor= "right")
-            )
-        
-    } else {
-        plot_data <- tribble(
-            ~year, ~all_percentage
-        )
+            nrow()
 
-        for (current_year in years) {
+        umc_denom <-  dataset %>%
+            filter(
+                city == umc,
+                year == current_year
+            ) %>%
+            nrow()
 
-            all_numer <-  dataset %>%
-                filter(
-                    year == current_year,
-                    published_5a
-                ) %>%
-                nrow()
+        all_numer <-  dataset %>%
+            filter(
+                year == current_year,
+                published_5a
+            ) %>%
+            nrow()
 
-            all_denom <-  dataset %>%
-                filter(
-                    year == current_year
-                ) %>%
-                nrow()
-            all_percentage <- 100*all_numer/all_denom
+        all_denom <-  dataset %>%
+            filter(
+                year == current_year
+            ) %>%
+            nrow()
+
+        umc_percentage <- 100*umc_numer/umc_denom
+        all_percentage <- 100*all_numer/all_denom
+
+        if (all_denom > 5) { ## This is because we only have 1
+            ## data point in 2013 with 5 years of
+            ## follow-up
 
             plot_data <- plot_data %>%
                 bind_rows(
                     tribble(
-                        ~year, ~all_percentage,
-                        current_year, all_percentage
+                        ~year, ~umc_percentage, ~all_percentage,
+                        current_year, umc_percentage, all_percentage
                     )
                 )
-            
         }
-
-        plot_ly(
-            plot_data,
-            x = ~year,
-            y = ~all_percentage,
-            name = umc,
-            type = 'scatter',
-            mode = 'lines+markers',
-            marker = list(
-                color = color_palette[3],
-                line = list(
-                    color = 'rgb(0,0,0)',
-                    width = 1.5
-                )
-            )
-        ) %>%
-            layout(
-                xaxis = list(
-                    title = '<b>UMC</b>',
-                    dtick = 1
-                ),
-                yaxis = list(
-                    title = '<b>Reported within 5 years (%)</b>',
-                    range = c(0, 100)
-                ),
-                paper_bgcolor = color_palette[9],
-                plot_bgcolor = color_palette[9],
-                legend = list(xanchor= "right")
-            )
-        
         
     }
+
+    plot_ly(
+        plot_data,
+        x = ~year,
+        y = ~umc_percentage,
+        name = umc,
+        type = 'scatter',
+        mode = 'lines+markers',
+        marker = list(
+            color = color_palette[3],
+            line = list(
+                color = 'rgb(0,0,0)',
+                width = 1.5
+            )
+        )
+    ) %>%
+        add_trace(
+            y=~all_percentage,
+            name='All',
+            marker = list(color = color_palette[2])
+        ) %>%
+        layout(
+            xaxis = list(
+                title = '<b>Completion year</b>',
+                dtick = 1
+            ),
+            yaxis = list(
+                title = '<b>Reported within 5 years (%)</b>',
+                range = c(0, 100)
+            ),
+            paper_bgcolor = color_palette[9],
+            plot_bgcolor = color_palette[9],
+            legend = list(xanchor= "right")
+        )
     
 }
 
