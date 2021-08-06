bce36c0ae84591164d2f4ebf3b18815c453ff68f
bgcarlisle <bgcarlisle@users.noreply.github.com>
2021-08-06 14:08:52 +0200

    Trying the height attribute

--- why_page.R
+++ why_page.R
@@ -2,5 +2,5 @@ why_page <- tabPanel(
     "Why these metrics?",
     value = "tabWhy",
     h2("Why these metrics?"),
-    img(src="why-these-metrics-dashboard.png", style="width: 100%;")
+    img(src="why-these-metrics-dashboard.png", height = "100%")
 )
