6e3e10fcf1d7af30daeb737c90b733045601836e
Delwen Franzen <delwen@franzen.fm>
2022-02-03 18:27:20 +0100

    Refer to practices in why page

--- why_page.R
+++ why_page.R
@@ -1,6 +1,6 @@
 why_page <- tabPanel(
-    "Why these metrics?",
+    "Why these practices?",
     value = "tabWhy",
-    h2("Why these metrics?"),
+    h2("Why these practices?"),
     img(src="why-these-metrics-dashboard.png", width = "100%")
 )
